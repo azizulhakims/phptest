e6f75c5b06df91f49a3febf91abd83b713a9e3b3
azizulhakims <it.azizulhakim@gmsil.com>
2023-02-11 17:19:51 +0600

    add loop

---
+++ loop.php
@@ -0,0 +1,11 @@
+<?php
+for ($i = 0; $i <= 5; $i++) {
+    for ($j = 0; $j < $i; $j++) {
+        echo '*';
+    }
+    echo "\n";
+}
+
+// for ($i = 0; $i < 5; $i++) {
+//     echo 'hello' . "\n";
+// }
